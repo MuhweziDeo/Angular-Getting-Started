e57fde45ee5f92561eb74c0dbeae5af486b3ea3c
muhwezi deo <aggrey256@gmail.com>
2019-03-24 15:37:19 +0300

    setup routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector:'pm-root',
   template:`
-  <div>
-  <h1>{{pageTitle}}</h1>
-  <pm-products></pm-products>
+  <nav class="navbar navbar-expand-sm bg-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" [routerLink]="['/welcome']">Dee Product Management</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" [routerLink]="['/products']">ProductList</a>
+      </li>
+      
+    </ul>
+  
+  </nav>
+  <div class="container">
+  <router-outlet></router-outlet>
   </div>
   
   `
