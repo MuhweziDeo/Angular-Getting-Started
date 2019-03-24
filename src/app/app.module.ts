3fdbace5a0c6f9a3893f4145c3ef9561ba7212ba
muhwezi deo <aggrey256@gmail.com>
2019-03-24 17:10:52 +0300

    more on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { WelcomeComponent } from './home/welcome.component';
     HttpClientModule,
     RouterModule.forRoot([
       {path:'products',component:ProductListComponent},
-      {path:'products/id',component:ProductDetailComponent},
+      {path:'products/:id',component:ProductDetailComponent},
       {path:'welcome', component:WelcomeComponent},
       {path:'',redirectTo:'welcome',pathMatch:'full'},
       {path:'**',redirectTo:'welcome',pathMatch:'full'},
--- src/app/product-list/product-detail.component.ts
+++ src/app/product-list/product-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProducts } from './products'
 @Component({
   // selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  product: IProducts,
+  pageTitle:string="product"
+  constructor(private route : ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
+    // console.log(this.route.params._value.id)
+    let id = this.route.snapshot.paramMap.get('id')
+    console.log(id)
+    this.product ={
+      "productId": parseInt(id),
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    }
   }
-
+  onBack():void{
+      this.router.navigate(['/products']);
+  }
+  
 }
